c9166d1beb770a9c39d2ab95ae2c638226f71084
Nonso <nonsoiwedinobi@yahoo.com>
2023-11-17 11:32:42 +0000

    Implemented local storage function

--- starter/index.js
+++ starter/index.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
     $(".saveBtn").on("click", function () {
       let appointmentDetail = $(this).siblings(".description").val()
       let appointmentHour = $(this).parent().attr("id");
-
+      console.log("appointmentDetail = ", appointmentDetail)
+      console.log("appointmentHour = ", appointmentHour)
       localStorage.setItem(appointmentHour, appointmentDetail);
     })
   });
