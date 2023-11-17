8037a5a46315cb7676065a7b33aaba5174dddd0a
Nonso <nonsoiwedinobi@yahoo.com>
2023-11-17 11:41:08 +0000

    Included a function to persitently reload saved appointments

--- starter/index.js
+++ starter/index.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var currentDate = dayjs().format("dddd, MMMM D[th]");
     $("#currentDay").text(currentDate);
 
+    // Save appointment details to local storage on saveBtn click
     $(".saveBtn").on("click", function () {
       let appointmentDetail = $(this).siblings(".description").val()
       let appointmentHour = $(this).parent().attr("id");
@@ -10,6 +11,17 @@ $(document).ready(function() {
       console.log("appointmentHour = ", appointmentHour)
       localStorage.setItem(appointmentHour, appointmentDetail);
     })
+
+    // Restore from local storage and update DOM on page reload
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
   });
 
   
