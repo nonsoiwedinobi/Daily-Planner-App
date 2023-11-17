088a46cdf786e7ed910e607c584cda89959b52b8
Nonso <nonsoiwedinobi@yahoo.com>
2023-11-17 11:14:47 +0000

    Added event listener function

--- starter/index.js
+++ starter/index.js
@@ -3,6 +3,12 @@ $(document).ready(function() {
     var currentDate = dayjs().format("dddd, MMMM D[th]");
     $("#currentDay").text(currentDate);
 
+    $(".saveBtn").on("click", function () {
+      let appointmentDetail = $(this).siblings(".description").val()
+      let appointmentHour = $(this).parent().attr("id");
+
+      localStorage.setItem(appointmentHour, appointmentDetail);
+    })
   });
 
   
